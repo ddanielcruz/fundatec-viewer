61f1c29a12e02a6ecd9cf7f33ebcdc07cc138313
Daniel Cruz <danielcunha54@gmail.com>
2024-12-17 10:38:58 -0300

    Add shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
